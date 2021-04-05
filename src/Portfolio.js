87fc496bdf50602ea9c539dd827341e8f621fd71
PatrickBoynton <jpboynton3@gmail.com>
2021-04-05 12:57:40 -0400

    Added boilerplate.

---
+++ src/Portfolio.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class Portfolio extends Component{
+    render() {
+        return (
+            <div>
+                Component
+            </div>
+        );
+    }
+}
+
+export default Portfolio
