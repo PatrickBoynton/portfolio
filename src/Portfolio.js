823570f22f5fb312e42302155e4d1e819a22d198
PatrickBoynton <jpboynton3@gmail.com>
2021-04-05 13:36:42 -0400

    Added test images for the component.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,13 +1,21 @@
-import {Component} from 'react';
+import {Component} from "react";
 
-class Portfolio extends Component{
+class Portfolio extends Component {
     render() {
         return (
-            <div>
-                Component
+            <div className="container">
+                <div className="row">
+                    <h2>Portfolio</h2>
+                    <div className="col-12">
+                        <img src={process.env.PUBLIC_URL + '/Code1.jpeg'} alt="Website"/>
+                        <img src={process.env.PUBLIC_URL + '/Code2.jpeg'} alt="Website"/>
+                        <img src={process.env.PUBLIC_URL + '/Code3.jpeg'} alt="Website"/>
+                        <img src={process.env.PUBLIC_URL + '/Code4.jpeg'} alt="Website"/>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Portfolio
+export default Portfolio;
