4e132c1803c9b8b9bb8c33960c6849e662daac74
PatrickBoynton <jpboynton3@gmail.com>
2021-04-05 13:36:19 -0400

    Added the portfolio component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {Component} from "react";
+import Portfolio from "./Portfolio";
 
 class App extends Component {
   render() {
     return (
-        <div>App</div>
+        <>
+          <Portfolio/>
+        </>
     );
   }
 }
