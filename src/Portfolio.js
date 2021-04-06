e9a85ed7f63f5fb2d4c3144ae055a906817245ce
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 10:31:21 -0400

    Trying out basic louts.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,16 +3,12 @@ import {Component} from "react";
 class Portfolio extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <h2>Portfolio</h2>
-                    <div className="col-12">
-                        <img src={process.env.PUBLIC_URL + '/Code1.jpeg'} alt="Website"/>
-                        <img src={process.env.PUBLIC_URL + '/Code2.jpeg'} alt="Website"/>
-                        <img src={process.env.PUBLIC_URL + '/Code3.jpeg'} alt="Website"/>
-                        <img src={process.env.PUBLIC_URL + '/Code4.jpeg'} alt="Website"/>
-                    </div>
-                </div>
+            <div className="col-6">
+                <h2>Portfolio</h2>
+                <img src={process.env.PUBLIC_URL + "/Code1.jpeg"} alt="Website"/>
+                <img src={process.env.PUBLIC_URL + "/Code2.jpeg"} alt="Website"/>
+                <img src={process.env.PUBLIC_URL + "/Code3.jpeg"} alt="Website"/>
+                <img src={process.env.PUBLIC_URL + "/Code4.jpeg"} alt="Website"/>
             </div>
         );
     }
