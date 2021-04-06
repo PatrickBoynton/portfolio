93382223cbf86db329794cd06edf20a01ff6cd21
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 12:49:34 -0400

    Added icons.

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,15 @@ class Footer extends Component {
         return (
             <div className="text-center">
                 <h2>Contact</h2>
-                <ul className="list-group d-flex">
-                    <li className="list-group-item" >LinkedIn</li>
-                    <li className="list-group-item" >Facebook</li>
-                    <li className="list-group-item" >GitHub</li>
+                <ul className="list-group">
+                    <li style={{color: '#0A66C2'}} className="list-group-item"><i className="fa fa-linkedin" aria-hidden="true"></i></li>
+                    <li style={{color: '#0E8DF1'}} className="list-group-item"><i className="fa fa-facebook-square" aria-hidden="true"></i></li>
+                    <li className="list-group-item"><i className="fa fa-github" aria-hidden="true"></i></li>
+                    <li className="list-group-item"><i className="fa fa-envelope" aria-hidden="true"></i></li>
                 </ul>
             </div>
         );
     }
 }
 
-export default Footer
+export default Footer;
