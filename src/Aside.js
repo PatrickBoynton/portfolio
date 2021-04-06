05d398af7cfbb1d18ecf921c736f720fa0854d9f
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 10:25:54 -0400

    Removed the bullet points.

--- src/Aside.js
+++ src/Aside.js
@@ -3,8 +3,16 @@ import {Component} from 'react';
 class Aside extends Component {
     render() {
         return (
-            <div>
-                Aside
+            <div className="col-2">
+                <h2>Tech skills</h2>
+                <ul className="list-group">
+                    <li className="list-group-item">HTML</li>
+                    <li className="list-group-item" >CSS</li>
+                    <li className="list-group-item">JavaScript</li>
+                    <li className="list-group-item" >React</li>
+                    <li className="list-group-item">Python</li>
+                    <li className="list-group-item" >Django</li>
+                </ul>
             </div>
         );
     }
