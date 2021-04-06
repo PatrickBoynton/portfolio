a7b6b71f4c13d19734f802a940ea7e5d8a8ea50a
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 12:11:08 -0400

    Added content.

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,13 @@ import {Component} from "react";
 class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
+            <div className="text-center">
+                <h2>Contact</h2>
+                <ul className="list-group d-flex">
+                    <li className="list-group-item" >LinkedIn</li>
+                    <li className="list-group-item" >Facebook</li>
+                    <li className="list-group-item" >GitHub</li>
+                </ul>
             </div>
         );
     }
