487783e13455973a32f98560d0ef96a5b0baf519
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 12:47:17 -0400

    Added an education section.

---
+++ src/Education.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+
+class Education extends Component{
+    render() {
+        return (
+            <div className="education">
+                <h3>Education</h3>
+                <ul className="list-group">
+                    <li className="list-group-item" >Associates in Science Greenville Technical College</li>
+                    <li className="list-group-item" >Carolina Code School Graduate</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Education
