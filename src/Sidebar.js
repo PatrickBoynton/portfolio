91f50bc0555d6053be681c081f7bed621404076c
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 08:30:09 -0400

    Created temporary html.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,9 @@ class Sidebar extends Component{
     render() {
         return (
             <div>
-                Sidebar
+                <h2>Patrick Boynton</h2>
+                <img src={process.env.PUBLIC_URL + '/person-placeholder.jpeg'} alt="Me"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad deleniti dolorum facere facilis, perspiciatis porro quam quas sapiente sit!</p>
             </div>
         );
     }
