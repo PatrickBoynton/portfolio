8aff4f8b461c01871166a9d95537a8e4750db377
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 10:31:30 -0400

    Trying out basic louts.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 class Sidebar extends Component{
     render() {
         return (
-            <div>
+            <div className="text-center col-2">
                 <h2>Patrick Boynton</h2>
                 <img src={process.env.PUBLIC_URL + '/person-placeholder.jpeg'} alt="Me"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad deleniti dolorum facere facilis, perspiciatis porro quam quas sapiente sit!</p>
