7fd6f5a94b902d8f0dce29215b0900dfdf254101
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 08:28:37 -0400

    Added an aside and sidebar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import "./App.css";
 import {Component} from "react";
 import Portfolio from "./Portfolio";
+import Aside from "./Aside";
+import Sidebar from "./Sidebar";
 
 class App extends Component {
-  render() {
-    return (
-        <>
-          <Portfolio/>
-        </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Sidebar/>
+                <Portfolio/>
+                <Aside/>
+            </>
+        );
+    }
 }
 
 export default App;
