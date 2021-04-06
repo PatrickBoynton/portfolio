b2ae2b038f915b9eb750a1bdf73fc747e4b578c6
PatrickBoynton <jpboynton3@gmail.com>
2021-04-06 12:12:35 -0400

    Added classes to the image.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,10 @@ import {Component} from 'react';
 class Sidebar extends Component{
     render() {
         return (
-            <div className="text-center col-2">
+            <div className="text-center col-4">
                 <h2>Patrick Boynton</h2>
-                <img src={process.env.PUBLIC_URL + '/person-placeholder.jpeg'} alt="Me"/>
+
+                <img className="rounded-circle my-pic mt-3 mb-3" src={`${process.env.PUBLIC_URL}/person-placeholder.jpeg`} alt="Me"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad deleniti dolorum facere facilis, perspiciatis porro quam quas sapiente sit!</p>
             </div>
         );
