e6a55fbd06823a6701fbf5ede90a44fac7e0f7f9
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 09:17:24 -0400

    Changed styling.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,10 +5,10 @@ import Footer from "./Footer";
 class Sidebar extends Component{
     render() {
         return (
-            <div className="text-center col-4">
+            <div className="text-center col-3">
                 <h2>Patrick Boynton</h2>
 
-                <img className="rounded-circle my-pic mt-3 mb-3" src={`${process.env.PUBLIC_URL}/person-placeholder.jpeg`} alt="Me"/>
+                <img style={{width: '60%'}} className="rounded-circle my-pic mt-3 mb-3" src={`${process.env.PUBLIC_URL}/person-placeholder.jpeg`} alt="Me"/>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad deleniti dolorum facere facilis, perspiciatis porro quam quas sapiente sit!</p>
                 <Education/>
                 <Footer/>
