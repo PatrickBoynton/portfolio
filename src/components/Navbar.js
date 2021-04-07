b23427bec48b4ee1c47f2c47464d95b47bea0dae
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 17:54:31 -0400

    Added a navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import {Component} from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="p-3 navbar-dark">
+                <ul className="nav navbar-collapse justify-content-center">
+                    <li><a href="#">About</a></li>
+                    <li className="pl-5"><a href="#">My Work</a></li>
+                    <li className="pl-5"><a href="#">Contact</a></li>
+                    <li className="pl-5"><a href="#">Resume</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
