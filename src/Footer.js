7aea752662c6f090ed4b8fe20dadf871d8686a9d
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 07:58:13 -0400

    Added styling to the li's.

--- src/Footer.js
+++ src/Footer.js
@@ -6,10 +6,10 @@ class Footer extends Component {
             <div className="text-center">
                 <h2>Contact</h2>
                 <ul className="list-group d-flex flex-row justify-content-between">
-                    <li className="list-group-item" style={{color: '#0A66C2', border: 'none'}}><i className="fa fa-linkedin list-group-item" aria-hidden="true"></i></li>
-                    <li className="list-group-item" style={{color: '#0E8DF1', border: 'none'}}><i className="fa fa-facebook-square list-group-item" aria-hidden="true"></i></li>
-                    <li className="list-group-item" style={{border: 'none'}}><i className="fa fa-github list-group-item" aria-hidden="true"></i></li>
-                    <li className="list-group-item" style={{color:'#BB0000', border: 'none'}}><i className="fa fa-envelope list-group-item" aria-hidden="true"></i></li>
+                    <li className="list-group-item" style={{color: '#0A66C2', border: 'none'}}><i className="fa fa-linkedin list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{color: '#0E8DF1', border: 'none'}}><i className="fa fa-facebook-square list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{border: 'none'}}><i className="fa fa-github list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{color:'#BB0000', border: 'none'}}><i className="fa fa-envelope list-group-item" aria-hidden="true"/></li>
                 </ul>
             </div>
         );
