f342ce16ca7fbb5f68b7122a1e038b35f75afc37
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 10:48:23 -0400

    Added a m-5. Removed the aside.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
 import {Component} from "react";
 import Portfolio from "./Portfolio";
-import Aside from "./Aside";
 import Sidebar from "./Sidebar";
 
 class App extends Component {
     render() {
         return (
             <>
-                <div className="row mt-5 mx-4">
+                <div className="row m-5">
                     <Sidebar/>
                     <Portfolio/>
                 </div>
-                <Aside/>
             </>
         )
             ;
