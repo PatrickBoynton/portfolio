d24745b3af2f26082776952411e0f1fbffb2787e
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 07:57:38 -0400

    Formatting.

--- src/Aside.js
+++ src/Aside.js
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {Component} from "react";
 
 class Aside extends Component {
     render() {
@@ -6,20 +6,22 @@ class Aside extends Component {
             <div className="col-2 text-center">
                 <h2>Tech skills</h2>
                 <ul className="list-group">
-                    <li style={{border: 'none'}} className="list-group-item">HTML</li>
-                    <li style={{border: 'none'}} className="list-group-item" >CSS</li>
-                    <li style={{border: 'none'}} className="list-group-item">JavaScript</li>
-                    <li style={{border: 'none'}} className="list-group-item" >React</li>
-                    <li style={{border: 'none'}} className="list-group-item">Python</li>
-                    <li style={{border: 'none'}} className="list-group-item" >Django</li>
+                    <li style={{border: "none"}} className="list-group-item">HTML</li>
+                    <li style={{border: "none"}} className="list-group-item">CSS</li>
+                    <li style={{border: "none"}} className="list-group-item">JavaScript</li>
+                    <li style={{border: "none"}} className="list-group-item">React</li>
+                    <li style={{border: "none"}} className="list-group-item">Python</li>
+                    <li style={{border: "none"}} className="list-group-item">Django</li>
                 </ul>
                 <h2>Soft Skills</h2>
-                <li style={{border: 'none'}} className="list-group-item">Hard Worker</li>
-                <li style={{border: 'none'}} className="list-group-item">Team Player</li>
-                <li style={{border: 'none'}} className="list-group-item">Problem Solver</li>
+                <ul className="list-group">
+                    <li style={{border: "none"}} className="list-group-item">Hard Worker</li>
+                    <li style={{border: "none"}} className="list-group-item">Team Player</li>
+                    <li style={{border: "none"}} className="list-group-item">Problem Solver</li>
+                </ul>
             </div>
         );
     }
 }
 
-export default  Aside
+export default Aside;
