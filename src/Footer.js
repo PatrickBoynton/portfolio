531f14976ea4e480dbccb46fa65e6b7b84fca2a0
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 09:15:16 -0400

    Fixed the spacing issue.

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,11 @@ class Footer extends Component {
         return (
             <div className="text-center">
                 <h2>Contact</h2>
-                <ul className="list-group d-flex flex-row justify-content-between">
-                    <li className="list-group-item" style={{color: '#0A66C2', border: 'none'}}><i className="fa fa-linkedin list-group-item" aria-hidden="true"/></li>
-                    <li className="list-group-item" style={{color: '#0E8DF1', border: 'none'}}><i className="fa fa-facebook-square list-group-item" aria-hidden="true"/></li>
-                    <li className="list-group-item" style={{border: 'none'}}><i className="fa fa-github list-group-item" aria-hidden="true"/></li>
-                    <li className="list-group-item" style={{color:'#BB0000', border: 'none'}}><i className="fa fa-envelope list-group-item" aria-hidden="true"/></li>
+                <ul className="list-group flex-row justify-content-center">
+                    <li className="list-group-item" style={{color: '#0A66C2', border: 'none', padding:'0'}}><i className="fa fa-linkedin list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{color: '#0E8DF1', border: 'none', padding:'0'}}><i className="fa fa-facebook-square list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{border: 'none', padding:'0'}}><i className="fa fa-github list-group-item" aria-hidden="true"/></li>
+                    <li className="list-group-item" style={{color:'#BB0000', border: 'none', padding:'0'}}><i className="fa fa-envelope list-group-item" aria-hidden="true"/></li>
                 </ul>
             </div>
         );
