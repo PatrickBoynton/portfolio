6edf80f4c30719d995763bee97835231d13260a0
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 12:47:31 -0400

    Removed the test div.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import {Component} from "react";
 import Portfolio from "./Portfolio";
 import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
 class App extends Component {
     render() {
         return (
             <>
-                <div className="row m-5">
-                    <Sidebar/>
-                    <Portfolio/>
-                </div>
+                <Sidebar/>
+                <Portfolio/>
+                <Footer/>
             </>
         )
             ;
