004fffad58e5d5dc5b70a01e6a7630c8a324c185
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 00:34:28 -0400

    Removed the border. Added more content.

--- src/Aside.js
+++ src/Aside.js
@@ -3,16 +3,20 @@ import {Component} from 'react';
 class Aside extends Component {
     render() {
         return (
-            <div className="col-2">
+            <div className="col-2 text-center">
                 <h2>Tech skills</h2>
                 <ul className="list-group">
-                    <li className="list-group-item">HTML</li>
-                    <li className="list-group-item" >CSS</li>
-                    <li className="list-group-item">JavaScript</li>
-                    <li className="list-group-item" >React</li>
-                    <li className="list-group-item">Python</li>
-                    <li className="list-group-item" >Django</li>
+                    <li style={{border: 'none'}} className="list-group-item">HTML</li>
+                    <li style={{border: 'none'}} className="list-group-item" >CSS</li>
+                    <li style={{border: 'none'}} className="list-group-item">JavaScript</li>
+                    <li style={{border: 'none'}} className="list-group-item" >React</li>
+                    <li style={{border: 'none'}} className="list-group-item">Python</li>
+                    <li style={{border: 'none'}} className="list-group-item" >Django</li>
                 </ul>
+                <h2>Soft Skills</h2>
+                <li style={{border: 'none'}} className="list-group-item">Hard Worker</li>
+                <li style={{border: 'none'}} className="list-group-item">Team Player</li>
+                <li style={{border: 'none'}} className="list-group-item">Problem Solver</li>
             </div>
         );
     }
