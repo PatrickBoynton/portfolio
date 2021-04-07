3b9f54db688eda6c1fce818f27c43ba57e0cf62f
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 18:37:41 -0400

    Added the about and footer.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import {Component} from "react";
 import Navbar from "./components/Navbar";
+import About from './components/About';
+import Footer from './components/Footer';
 
 class App extends Component {
     render() {
         return (
             <>
                 <Navbar/>
+                <About/>
+                <Footer/>
             </>
         )
             ;
