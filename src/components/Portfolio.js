221012b63fb21cba17b0ebbd7a9c8be05ca460f7
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 10:51:24 -0400

    Refactored the portfolio.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,40 +1,33 @@
 import {Component} from "react";
+import Card from "./Card";
 
 class Portfolio extends Component {
     render() {
         return (
             <div className="row align-baseline p-4" style={{height: "85vh"}}>
-                <div className="card w-25 h-75 m-4 text-center">
-                    <img src={`${process.env.PUBLIC_URL}/NW.png`} className="card-img-top" alt="Night Watch"/>
-                    <div className="card-body">
-                        <h3 className="card-title">Night Watch</h3>
-                        <p className="card-text">Night Watch is an application where the user signs up and gets
-                            notifications via text to tell them when a planet star, or other celestial event is about to
-                            happen. Tech used was Django, Celery (For automating texts and rise and set times), Skyfield
-                            API (For calculating rise and set times) and React for the UI.</p>
-                    </div>
-                </div>
+                <Card image='/NW.png'
+                      title='Night Watch'
+                      alt='Night Watch'
+                      text='Night Watch is an application where the user signs up and gets
+                        notifications via text to tell them when a planet star, or other celestial event is about to
+                        happen. Tech used was Django, Celery (For automating texts and rise and set times), Skyfield
+                        API (For calculating rise and set times) and React for the UI.'/>
 
-                <div className="card w-25 h-75 m-4 text-center">
-                    <img src={`${process.env.PUBLIC_URL}/Code2.jpeg`} className="card-img-top" alt="Night Watch"/>
-                    <div className="card-body">
-                        <h3 className="card-title">Galaxy News</h3>
-                        <p className="card-text">Galaxy News is the first Django/React project I created using custom
+                 <Card image='/Code2.jpeg'
+                       title='Galaxy News'
+                       alt='Galaxy News'
+                       text='Galaxy News is the first Django/React project I created using custom
                             users, and filtering by some sort of status. There was no external technology used, but the
                             challenge of filtering by different things and the user accounts made this memorable. It was
-                            also my first experience with React Router.</p>
-                    </div>
-                </div>
+                            also my first experience with React Router.'/>
 
-                <div className="card w-25 h-75 m-4 text-center">
-                    <img src={`${process.env.PUBLIC_URL}/PixelPerfect.png`} className="card-img-top" alt="Night Watch"/>
-                    <div className="card-body">
-                        <h3 className="card-title">Pixel Perfect</h3>
-                        <p className="card-text">The first really big project I did, it took quite awhile, and while it
+                <Card image='/PixelPerfect.png'
+                      alt='Pixel Perfect'
+                      title='Pixel Perfect'
+                      text='The first really big project I did, it took quite awhile, and while it
                             may not be exactly pixel perfect, I think it looks good enough for using little more
-                            than HTML and CSS. Every bit of this was copied as nearly as exactly as I could get the text.</p>
-                    </div>
-                </div>
+                            than HTML and CSS. Every bit of this was copied as nearly as exactly as I could get the text.'/>
+
             </div>
         );
     }
