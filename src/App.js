5e7303191aaee2869ba17c202b0ce1575eea9bf5
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 20:20:26 -0400

    Added routing.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import {Component} from "react";
 import Navbar from "./components/Navbar";
-import About from './components/About';
-import Footer from './components/Footer';
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+import {Switch, Route} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <>
                 <Navbar/>
-                <About/>
+                <Switch>
+                    <Route path="/works" component={Portfolio}/>
+                    <Route path="/resume" component={Resume}/>
+                    <Route path="/" component={About}/>
+                </Switch>
                 <Footer/>
             </>
         )
