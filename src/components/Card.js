df92c7fbd21fd700d51544460c1061c3f102896e
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 19:57:28 -0400

    Changed the header and added a class.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ class Card extends Component{
     render() {
         return (
             <div className="card w-25 h-75 m-4 text-center">
-                <img src={`${process.env.PUBLIC_URL}${this.props.image}`} className="card-img-top" alt={this.props.alt}/>
+                <img src={`${process.env.PUBLIC_URL}${this.props.image}`} className="card-img-top card-img" alt={this.props.alt}/>
                 <div className="card-body">
-                    <h3 className="card-title">{this.props.title}</h3>
+                    <h2 className="card-title">{this.props.title}</h2>
                     <p className="card-text">{this.props.text}</p>
                 </div>
             </div>
