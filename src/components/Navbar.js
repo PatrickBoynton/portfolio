831ed174b7625cfaf3150b00f0f23d1cbff42f47
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 12:13:36 -0400

    Changed the positioning.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 class Navbar extends Component {
     render() {
         return (
-            <nav className="p-3 navbar-dark">
+            <nav className="p-3 position-fixed fixed-top">
                 <ul className="nav navbar-collapse justify-content-center">
                     <li><NavLink exact to="/">About</NavLink></li>
                     <li className="pl-5"><NavLink to="/works">My Work</NavLink></li>
