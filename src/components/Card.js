4685f13faf4a9cfcf995d0d3d2ea1f4f37cab2da
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 10:50:28 -0400

    Made the card it's own component.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,12 @@ import {Component} from 'react';
 class Card extends Component{
     render() {
         return (
-            <div>
-               Card
+            <div className="card w-25 h-75 m-4 text-center">
+                <img src={`${process.env.PUBLIC_URL}${this.props.image}`} className="card-img-top" alt={this.props.alt}/>
+                <div className="card-body">
+                    <h3 className="card-title">{this.props.title}</h3>
+                    <p className="card-text">{this.props.text}</p>
+                </div>
             </div>
         );
     }
