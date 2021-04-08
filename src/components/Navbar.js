fa4cdafcf58857eeb94046c4e9283285936a8c23
PatrickBoynton <jpboynton3@gmail.com>
2021-04-07 20:23:11 -0400

    Added routes.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import {Component} from "react";
+import {NavLink} from "react-router-dom";
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="p-3 navbar-dark">
                 <ul className="nav navbar-collapse justify-content-center">
-                    <li><a href="#">About</a></li>
-                    <li className="pl-5"><a href="#">My Work</a></li>
-                    <li className="pl-5"><a href="#">Resume</a></li>
+                    <li><NavLink exact to="/">About</NavLink></li>
+                    <li className="pl-5"><NavLink to="/works">My Work</NavLink></li>
+                    <li className="pl-5"><NavLink to="/resume">Skills</NavLink></li>
                 </ul>
             </nav>
         );
