03fbcfacdbca70d68f06ea7237e699481c91ebbd
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 12:11:59 -0400

    Added a tooltip.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,13 @@ import {NavLink} from "react-router-dom";
 class Footer extends Component {
     render() {
         return (
-            <nav className="p-3 navbar-dark">
+            <nav className="p-3 position-fixed fixed-bottom">
                 <ul className="nav navbar-collapse justify-content-center">
                     <li><NavLink to="#"><i className="fa fa-linkedin" aria-hidden="true"/></NavLink></li>
                     <li className="pl-5"><NavLink to="#"><i className="fa fa-github" aria-hidden="true"/></NavLink></li>
-                    <li className="pl-5"><NavLink to="#"><i className="fa fa-file" aria-hidden="true"/></NavLink></li>
+                    <li className="pl-5"><NavLink to="#"><i className="fa fa-file" data-bs-toggle="tooltip"
+                                                            data-bs-placement="top" title="Click to download resume"
+                                                            aria-hidden="true"/></NavLink></li>
                 </ul>
             </nav>
         );
