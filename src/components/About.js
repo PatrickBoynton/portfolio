242a272f493254786f5698a424d612ba9883399f
PatrickBoynton <jpboynton3@gmail.com>
2021-04-08 12:10:36 -0400

    Added padding.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import {Component} from "react";
-import Footer from "./Footer";
 
 class About extends Component {
     render() {
@@ -8,7 +7,7 @@ class About extends Component {
                 <div style={{height: '85vh'}} className="d-flex">
                     <img style={{height: '80%'}} className="p-5 img-fluid rounded" src={`${process.env.PUBLIC_URL}/me.jpeg`} alt="Me"/>
                     <h1 style={{whiteSpace: 'nowrap'}} className="p-5">About Me</h1>
-                    <p style={{fontSize: "20px"}} className="p-5">For most of my adult life I worked for the City of
+                    <p className="p-5">For most of my adult life I worked for the City of
                         Greenville with Parking Services. At some point I
                         began wanting something more. I started talking to a friend of mine who told me that I didn't need a
                         CS Degree to earn a job programming which is something I've kind of always wanted to do for a
