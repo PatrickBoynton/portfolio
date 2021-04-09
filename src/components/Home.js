04ef5e01b2069accedbc69159cd38a4035e7429c
PatrickBoynton <jpboynton3@gmail.com>
2021-04-09 01:49:31 -0400

    Added content.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,24 @@
-import {Component} from 'react'
+import {Component} from "react";
 
-class Home  extends Component {
+class Home extends Component {
     render() {
         return (
-            <div>
-                Home
+            <div className="home-container d-flex">
+                <div className="square-1">
+                    <img className="main-img" src={`${process.env.PUBLIC_URL}/Main1.jpeg`} alt=""/>
+                    <h1 className="home-header">Patrick Boynton</h1>
+                </div>
+                <div className="square-2">
+                    <p className="home-text">
+                        I am a passionate programmer, who loves solving problems and helping people. I recently graduated
+                        from Carolina Code School, and learned a lot about myself and the technologies needed to be a
+                        succesful programmer in today's world. If you want to find out more, either click the about, or
+                        skills at the top of the page. If you are looking for a resume, click the skills page.
+                    </p>
+                </div>
             </div>
         );
     }
 }
 
-export default Home
+export default Home;
