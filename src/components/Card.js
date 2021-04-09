3ebf31c1bfa1f9bbd9afbf7e155ec531777027bc
PatrickBoynton <jpboynton3@gmail.com>
2021-04-09 01:49:18 -0400

    Removed the text-center.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 class Card extends Component{
     render() {
         return (
-            <div className="card w-25 h-75 m-4 text-center">
+            <div className="card w-25 h-75 m-4">
                 <img src={`${process.env.PUBLIC_URL}${this.props.image}`} className="card-img-top card-img" alt={this.props.alt}/>
                 <div className="card-body">
                     <h2 className="card-title">{this.props.title}</h2>
