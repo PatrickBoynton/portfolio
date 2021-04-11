1f832fac7e5e325598bb387564c3597fcf4233b7
PatrickBoynton <jpboynton3@gmail.com>
2021-04-11 11:46:46 -0400

    Changed the opening text.

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,21 +11,13 @@ class Home extends Component {
                 </aside>
                 <div className="d-flex flex-column">
                     <h1 className="home-header-2">About Me</h1>
-                    <p className="home-text">My name is Patrick, I am a recent graduate of Carolina Code School. Before now I
-                        worked for the City of Greenville Parking Services for many years. It was a good job, but
-                        eventually I felt I could do more, and wanted more than a part time job that didn't seem that
-                        fulfilling.I have as far back as I can remember I have been interested in technology, specifically
-                        programming, because I enjoy the challenges that it can throw at you. In the past I have always
-                        considered it a hobby and not something that I could do as a career, but not all that long ago a
-                        friend of mine who was then currently working as a software engineer said that a lot of
-                        companies were dropping the requirement of a CS Degree and that got me thinking that maybe I
-                        could finally do this as a career. I started teaching myself the basics of programming that I
-                        felt I should know to become a programmer. The path was difficult, and at the time there weren't
-                        as many resources as there are now, and eventually I got to the point where I felt I needed help
-                        getting the extra training I didn't know how to find. My brother originally went to the Iron
-                        Yard and got a job through that, and that's what led me to sign up for the course at Carolina
-                        Code School. I can already see the difference between how I coded before and after the
-                        class.</p>
+                    <p className="home-text">My name is Patrick, I am a recent graduate of Carolina Code School. Before
+                        now I worked for the City of Greenville. As far back as I can remember I have been interested in
+                        programming because I enjoy the challenges that it can throw at you. For years programming was a
+                        hobby, but a software engineer friend convinced me that I could do this as a career. I started
+                        teaching myself the basics of programming. When I wanted to expand my programming beyond what I
+                        could teach myself, I enrolled in Carolina Code School. I can already see the difference between
+                        how I coded before and after the class.</p>
                 </div>
             </section>
         );
