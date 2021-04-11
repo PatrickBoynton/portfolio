a20f28a757bb0e95472b001356d7655efcad8c84
PatrickBoynton <jpboynton3@gmail.com>
2021-04-11 11:47:20 -0400

    Styling the skills page.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,26 +3,33 @@ import {Component} from "react";
 class Resume extends Component {
     render() {
         return (
-            <section>
-                <h1>Education</h1>
-                <p>Associates in Science Greenville Technical College</p>
-                <p>Carolina Code School</p>
-                <h1>Tech Skills</h1>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Python</li>
-                    <li>Django</li>
-                    <li>Handlebars</li>
-                    <li>Bootstrap</li>
-                    <li>SQL (Postgres, MSSQL, SQLite)</li>
+            <section style={{margin: '10rem'}}>
+                <div className="education">
+                    <h1>Education</h1>
+                    <h2>Associates in Science Greenville Technical College</h2>
+                    <time style={{fontSize: '1.3rem'}}>Graduated March 2020</time>
+                    <h2>Carolina Code School</h2>
+                    <time style={{fontSize: '1.3rem'}}>Dec 13 2020 - April 2nd 2021</time>
+                </div>
+                    <h1>Tech Skills</h1>
+                <ul className="d-flex">
+                    <li style={{color: '#D35635'}}>HTML</li>
+                    <li style={{color: '#3077B8'}}> CSS</li>
+                    <li style={{color: '#D3C045'}}>JavaScript</li>
+                    <li style={{color: '#609BB0'}}> React</li>
+                    <li className="python"> Python</li>
+                    <li style={{color: '#3E7959'}}>Django</li>
+                    <h2>Extras: </h2>
+                    <li style={{color: '#DA773A'}}>Handlebars</li>
+                    <li style={{color: '#8B5CD0'}}>Bootstrap</li>
+                    <li style={{color: '#2A6EBA'}}>SQL (Postgres, MSSQL, SQLite)</li>
+                    <li style={{color: '#8D6EC9'}}>C#</li>
+                    <li style={{color: '#4836C6'}}>.Net Core/.Net</li>
                 </ul>
 
                 <h1>Soft Skills</h1>
                 <p>These are some, but not all of my soft skills.</p>
-                <ul>
+                <ul className="d-flex">
                     <li>Hard Worker</li>
                     <li>Problem Solver</li>
                     <li>Team Player</li>
