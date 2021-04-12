f7f4262ef08643b1cf9ae4b5c7c53438d47abeb1
PatrickBoynton <jpboynton3@gmail.com>
2021-04-12 11:17:20 -0400

    Changed the styling.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,24 +14,25 @@ class Resume extends Component {
                     <time style={{fontSize: "1.3rem"}}>Dec 13 2020 - April 2nd 2021</time>
                 </div>
                 <div className="d-flex skills mt-5 justify-content-center">
-                    <div className="text-center">
+                    <div>
                         <h1>Tech Skills</h1>
-                        <ul style={{marginBottom: '3.5rem'}}>
-                            <li style={{color: "#D35635"}}> <i className="fab fa-html5"/> HTML 5</li>
-                            <li style={{color: "#3077B8"}}> <i className="fab fa-css3"/> CSS 3</li>
-                            <li style={{color: "#D3C045"}}><i className="fab fa-js-square"/> JavaScript</li>
-                            <li style={{color: "#609BB0"}}> <i className="fab fa-react"/> React</li>
-                            <li className="python"> <i className="fab fa-python"/> Python</li>
-                            <li style={{color: "#3E7959"}}><i className="fab fa-python"/> Django</li>
+                        <ul style={{marginBottom: "3.5rem"}}>
+                            <li><i style={{color: "#D35635"}} className="fab fa-html5"/> HTML 5</li>
+                            <li><i style={{color: "#3077B8"}} className="fab fa-css3"/> CSS 3</li>
+                            <li><i style={{color: "#D3C045"}} className="fab fa-js-square"/> JavaScript</li>
+                            <li><i style={{color: "#609BB0"}} className="fab fa-react"/> React</li>
+                            <li><i className="fab fa-python python"/> Python</li>
+                            <li><i style={{color: "#3E7959"}} className="fab fa-python"/> Django</li>
                             <h2>Extras: </h2>
-                            <li style={{color: "#DA773A"}}>Handlebars</li>
-                            <li style={{color: "#8B5CD0"}}><i className="fab fa-bootstrap"/> Bootstrap</li>
-                            <li style={{color: "#2A6EBA"}}><i className="fa fa-database"/> SQL (Postgres, MSSQL, SQLite) and Redis</li>
-                            <li style={{color: "#8D6EC9"}}><i className="fab fa-microsoft"/> C#</li>
-                            <li style={{color: "#4836C6"}}><i className="fab fa-microsoft"/> .Net Core/.Net</li>
+                            <li><i style={{color: "#D35635"}} className="fab fa-js-square"/> Handlebars</li>
+                            <li><i style={{color: "#8B5CD0"}} className="fab fa-bootstrap"/> Bootstrap</li>
+                            <li><i style={{color: "#2A6EBA"}} className="fa fa-database"/> SQL</li>
+                            <li><i style={{color: "#88291F"}} className="fa fa-database"/> Redis</li>
+                            <li><i style={{color: "#8D6EC9"}} className="fab fa-microsoft"/> C#</li>
+                            <li><i style={{color: "#4836C6"}} className="fab fa-microsoft"/> .Net Core/.Net</li>
                         </ul>
                     </div>
-                    <div className="soft-skills text-center">
+                    <div className="soft-skills">
                         <h1>Soft Skills</h1>
                         <p>These are some, but not all of my soft skills.</p>
                         <ul>
