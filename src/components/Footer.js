2b39dacd1514cbd6f02997e6b5aee13cab6669f4
PatrickBoynton <jpboynton3@gmail.com>
2021-05-10 13:37:14 -0400

    Added my resume.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ class Footer extends Component {
         return (
             <nav className="p-3 position-fixed fixed-bottom">
                 <ul className="nav navbar-collapse justify-content-center">
-                    <li><a href="https://www.linkedin.com/in/patrick-boynton-a0a532119/"><i className="fab fa-linkedin"
+                    <li><a href="https://www.linkedin.com/in/patrick-boynton-a0a532119/"><i className="fab fa-3x fa-linkedin"
                                                                                             aria-hidden="true"/>
                     </a>
                     </li>
-                    <li className="pl-5"><a href="https://github.com/PatrickBoynton/"><i className="fab fa-github"
+                    <li className="pl-5"><a href="https://github.com/PatrickBoynton/"><i className="fab fa-3x fa-github"
                                                                                          aria-hidden="true"/>
                     </a>
                     </li>
-                    <li className="pl-5"><a href={process.env.PUBLIC_URL + '/resume1.docx'}><i className="fa fa-file" data-bs-toggle="tooltip"
+                    <li className="pl-5"><a href={process.env.PUBLIC_URL + '/resume1.docx'}><i className="fa fa-3x fa-file" data-bs-toggle="tooltip"
                                                         data-bs-placement="top" title="Click to download resume"
                                                         aria-hidden="true"/>
                     </a>
